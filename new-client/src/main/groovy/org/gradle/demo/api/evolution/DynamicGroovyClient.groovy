9e233c4e52a09161d186b822bc4665b56591ef36
Lóránt Pintér <lorant@gradle.com>
2021-08-27 13:25:30 +0200

    Separate set and get in dynamic Groovy

--- new-client/src/main/groovy/org/gradle/demo/api/evolution/DynamicGroovyClient.groovy
+++ new-client/src/main/groovy/org/gradle/demo/api/evolution/DynamicGroovyClient.groovy
@@ -2,11 +2,15 @@ package org.gradle.demo.api.evolution
 
 class DynamicGroovyClient {
     static void main() {
-        println run(new Server())
+        def server = new Server()
+        doSet(server)
+        println doGet(server)
     }
 
-    private static Object run(server) {
+    private static void doSet(Object server) {
         server.getTestProperty().set("lajos")
+    }
+    private static Object doGet(Object server) {
         return server.getTestProperty().get()
     }
 }
