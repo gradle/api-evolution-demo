1126a9312b29de98c0a15ffd089f3d9d1b7749f8
Lóránt Pintér <lorant@gradle.com>
2021-09-29 00:32:49 +0200

    Add tests for properties of all primitives and simple types

--- upgrader/src/test/groovy/org/gradle/demo/api/evolution/AbstractApiUpgradeSpec.groovy
+++ upgrader/src/test/groovy/org/gradle/demo/api/evolution/AbstractApiUpgradeSpec.groovy
@@ -4,9 +4,16 @@ import org.codehaus.groovy.control.CompilationUnit
 import org.codehaus.groovy.control.Phases
 import org.codehaus.groovy.runtime.EncodingGroovyMethods
 import org.codehaus.groovy.tools.GroovyClass
+import org.intellij.lang.annotations.Language
 import spock.lang.Specification
 
+/**
+ * Abstract base test class to introduce tools that allow testing old code
+ * compiled against other old code upgraded to work against new code.
+ */
 class AbstractApiUpgradeSpec extends Specification {
+    private static final String STATIC_SCRIPT_PREFIX = "import groovy.transform.*\n"
+
     protected ClassLoader originalClassLoader
     protected GroovyClassLoader oldClassLoader
     protected GroovyClassLoader newClassLoader
@@ -24,26 +31,38 @@ class AbstractApiUpgradeSpec extends Specification {
         Thread.currentThread().contextClassLoader = originalClassLoader
     }
 
-
-    protected Class<?> compileNew(String script) {
+    /**
+     * Compile a class against the new class-loader and then load it.
+     */
+    protected Class<?> compileNew(@Language(value = "groovy", prefix = STATIC_SCRIPT_PREFIX) String script) {
         def newClass = compile(newClassLoader, script)
         return newClassLoader.loadClass(newClass.name)
     }
 
-    protected Class<?> compileAndUpgradeOld(String script) {
+    /**
+     * Compile a class against the old class-loader, then re-define it
+     * in the new class-loader, then upgrade it, and then load it.
+     */
+    protected Class<?> compileAndUpgradeOld(@Language(value = "groovy", prefix = STATIC_SCRIPT_PREFIX) String script) {
         def oldClass = compile(oldClassLoader, script)
         manager.implementReplacements(newClassLoader, oldClass.bytes)
         return newClassLoader.loadClass(oldClass.name)
     }
 
-    protected Class<?> compileOld(String script) {
+    /**
+     * Compile a class against the old class-loader and then load it.
+     */
+    protected Class<?> compileOld(@Language(value = "groovy", prefix = STATIC_SCRIPT_PREFIX) String script) {
         def oldClass = compile(oldClassLoader, script)
         return oldClassLoader.loadClass(oldClass.name)
     }
 
-    private static GroovyClass compile(GroovyClassLoader classLoader, String script) {
+    private GroovyClass compile(GroovyClassLoader classLoader, String script) {
+        def fullScript = STATIC_SCRIPT_PREFIX + "import ${getClass().name}.*\n" + script
+        println "Compiling:\n" + fullScript
+
         def compileUnit = new CompilationUnit()
-        def sourceUnit = compileUnit.addSource("Script_" + EncodingGroovyMethods.md5(script) + ".groovy", script)
+        def sourceUnit = compileUnit.addSource("Script_" + EncodingGroovyMethods.md5(script) + ".groovy", fullScript)
         compileUnit.classLoader = classLoader
         compileUnit.compile(Phases.CLASS_GENERATION)
 
--- upgrader/src/test/groovy/org/gradle/demo/api/evolution/PropertyUpgradeTest.groovy
+++ upgrader/src/test/groovy/org/gradle/demo/api/evolution/PropertyUpgradeTest.groovy
@@ -1,57 +1,53 @@
 package org.gradle.demo.api.evolution
 
 import groovy.transform.CompileStatic
+import groovy.transform.EqualsAndHashCode
+import spock.lang.Unroll
 
+import java.lang.reflect.Type
+
+@Unroll
 class PropertyUpgradeTest extends AbstractApiUpgradeSpec {
 
-    def setup() {
-        compileNew """
-            @$CompileStatic.name
-            class Property<T> {
-                private T value
-                Property(T value) { set(value) }
-                T get() { value }
-                void set(T value) { this.value = value }
-            }
-        """
-    }
-
-    def "string property can be made lazy"() {
+    def "#originalType.simpleName property can be upgraded to lazy"() {
         def serverClass = compileNew """
-            @$CompileStatic.name
+            @CompileStatic
             class Server {
-                private final Property<String> property = new Property<String>("init")
-                Property<String> getString() { property }
+                final Property<$upgradedType.name> testProperty = new Property<>($originalValue)
             }
         """
-        manager.matchProperty(serverClass, String, "string")
+
+        manager.matchProperty(serverClass, originalType, "testProperty")
             .replaceWith(
-                { server -> server.getString().get() },
-                { server, value -> server.getString().set(value) }
+                { server -> server.getTestProperty().get() },
+                { server, value -> server.getTestProperty().set(value) }
             )
         manager.init()
 
         def oldServer = compileOld """
-            @$CompileStatic.name
+            @CompileStatic
             class Server {
-                private String value = "init"
-                String getString() {
+                private $originalType.name value = $originalValue
+                $originalType.name ${prefixFor(originalType)}TestProperty() {
                     value
                 }
-                void setString(String value) {
+                void setTestProperty($originalType.name value) {
                     this.value = value
                 }
             }
         """
+
         def oldClient = compileAndUpgradeOld """
-            @$CompileStatic.name
+            @CompileStatic
             class Client {
                 public void test() {
-                    println "Running test()"
                     def server = new Server()
-                    assert server.getString() == "init"
-                    server.setString("lajos")
-                    assert server.getString() == "lajos"
+                    assert server.${prefixFor(originalType)}TestProperty() == $originalValue
+                    server.setTestProperty($changedValue)
+                    assert server.${prefixFor(originalType)}TestProperty() == $changedValue
+
+                    server.testProperty = $originalValue
+                    assert server.testProperty == $originalValue
                 }
             }
         """
@@ -59,5 +55,51 @@ class PropertyUpgradeTest extends AbstractApiUpgradeSpec {
         oldClient.newInstance().test()
         then:
         noExceptionThrown()
+
+        where:
+        originalType | upgradedType | originalValue             | changedValue
+        boolean      | Boolean      | false                     | true
+        Boolean      | Boolean      | false                     | true
+        byte         | Byte         | '(byte) 0'                | '(byte) 123'
+        Byte         | Byte         | '(byte) 0'                | '(byte) 123'
+        short        | Short        | '(short) 0'               | '(short) 123'
+        Short        | Short        | '(short) 0'               | '(short) 123'
+        int          | Integer      | 0                         | 123
+        Integer      | Integer      | 0                         | 123
+        float        | Float        | '0F'                      | '123F'
+        Float        | Float        | '0F'                      | '123F'
+        char         | Character    | "(char) 'a'"              | "(char) 'b'"
+        Character    | Character    | "(${Character.name}) 'a'" | "(${Character.name}) 'b'"
+        String       | String       | '"original"'              | '"lajos"'
+        Thing        | Thing        | "new ${Thing.name}(1)"    | "new ${Thing.name}(2)"
+
+        // Long and double doesn't work yet
+//        long                 | Long                 | '0L'          | '123L'
+//        Long                 | Long                 | '0L'          | '123L'
+//        double               | Double               | '0D'          | '123D'
+//        Double               | Double               | '0D'          | '123D'
+    }
+
+    private static String prefixFor(Type type) {
+        type == boolean ? "is" : "get"
+    }
+
+    @CompileStatic
+    @EqualsAndHashCode
+    static class Thing {
+        private int value
+
+        Thing(int value) { this.value = value }
+    }
+
+    @CompileStatic
+    static class Property<T> {
+        private T value
+
+        Property(T value) { set(value) }
+
+        T get() { value }
+
+        void set(T value) { this.value = value }
     }
 }
